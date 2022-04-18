55920bd5276585e1b026a0e5c3600701780b81ba
VASTGAMING-YT <96776508+VASTGAMING-YT@users.noreply.github.com>
2022-04-18 16:51:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+song1="";
+song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function setup()
+{
+canvas=createCanvas(600, 500);
+canvas.center();
+
+video=createCapture(VIDEO);
+video.hide();
+
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function preload()
+{
+ song1=loadSound("MANIAC.mp3");
+ song2=loadSound("Charmer.mp3");
+}
+
+function draw()
+{
+image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+ song.play();
+}
+
+function modelLoaded()
+{
+console.log("PoseNet Is Initiallized");
+}
+
+function gotPoses(results)
+{
+if(results.length>0)
+{
+console.log(results);
+leftWristX=results[0].pose.leftWrist.x;
+leftWristY=results[0].pose.leftWrist.y;
+console.log("leftWristX = "+ leftWristX+ "leftWristY = "+ leftWristY);
+
+rightWristX=results[0].pose.rightWrist.x;
+rightWristY=results[0].pose.rightWrist.y;
+console.log("rightWristX = "+ rightWristX+ "rightWristY = "+ rightWristY);
+
+}
+}
